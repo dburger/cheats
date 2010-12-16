f87756f99da4c6787c18ea265ae34a647af8fcd5
David J. Burger <davidjburger@yahoo.com>
2010-12-15 20:10:03 -0800

    basic git commands added

--- cheats.git.sh
+++ cheats.git.sh
@@ -1,2 +1,13 @@
 # get helpful colors when possible
 git config --global color.ui auto
+
+# to put the branch back to state before last commit, will make those
+# changes not committed or staged, good for putting a WIP commit back
+# in the working tree
+git reset HEAD^
+
+# to back out all changes no longer staged or in working tree
+git reset --hard HEAD
+
+# to grab a specific file from another branch, will stage it
+git checkout BRANCH FILE
