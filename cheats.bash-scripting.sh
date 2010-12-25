e69032724138fde0703a3d7d9ee9796e5e2adf26
David J. Burger <loosedog@gmail.com>
2010-12-24 18:35:28 -0800

    add a getopts example

--- cheats.bash-scripting.sh
+++ cheats.bash-scripting.sh
@@ -1,10 +1,31 @@
-# prevent concurrent execution with flock
+# demonstrates how to prevent something like concurrent cron execution with
+# file locks via flock
+SCRIPT=$(basename "$0")
+
 (
-  flock -n 200
-  if [ $? -ne 0 ]; then
-    echo "unable to get the lock"
-    exit 1
-  fi
-  echo "got the lock"
-  sleep 40
-) 200>/var/lock/hourlymapreduce.lock
+flock -n 200
+if [ $? -ne 0 ]
+then
+  echo "Unable to run ${SCRIPT} because prior run has not finished." | \
+    mail -s "${SCRIPT} did not run" dburger@xxxxxx.xxx
+  exit 1
+fi
+
+echo "got the lock"
+sleep 60
+) 200>"/var/lock/${SCRIPT}.lock"
+
+
+# simple demo of basic getopts, for example
+# call with getopts -h localhost -e eth42
+hosts=
+eth=
+while getopts "h:e:" name; do
+  case $name in
+    h) hosts=$OPTARG;;
+    e) eth=$OPTARG;;
+  esac
+done
+
+echo "found ${hosts} and ${eth}"
+
