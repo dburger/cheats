804c14c3d8bf43bb455826d71f5290a97752d0f4
David J. Burger <davidjburger@yahoo.com>
2011-06-11 08:32:41 -0700

    adding permutations for good measure

--- cheats.R
+++ cheats.R
@@ -2,3 +2,8 @@
 combinations <- function(n, r) {
   return (factorial(n) / (factorial(r) * factorial(n - r)));
 }
+
+# and you can't show combinations without permutations
+permutations <- function(n, r) {
+  return (factorial(n) / factorial(n - r));
+}
